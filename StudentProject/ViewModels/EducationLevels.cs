c02c72149f05eb81c5b1ed0690f2f67f40b3d0f9
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-15 12:30:51 +0200

    EducationLevels osztály a diák érettségi típusainak tárolására

---
+++ StudentProject/ViewModels/EducationLevels.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace StudentProject.ViewModels
+{
+    public class EducationLevels
+    {
+        public List<string> AllEducationLevels { get; } = new List<string> { "érettségi", "szakmai érettségi", "szakmai vizsga" };
+    }
+}
