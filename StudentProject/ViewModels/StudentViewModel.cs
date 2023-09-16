44e64e43e0a4c665864080b910a128315701bc0c
gycsaba <gyuris.csaba@vasvari.org>
2023-09-16 12:00:21 +0200

    Kész

--- StudentProject/ViewModels/StudentViewModel.cs
+++ StudentProject/ViewModels/StudentViewModel.cs
@@ -1,27 +1,51 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using StudentProject.Models;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Navigation;
 
 namespace StudentProject.ViewModels
 {
     public partial class StudentViewModel : ObservableObject
     {
-        [ObservableProperty]
-        private Student student;
+        // https://egvijayanand.in/2022/04/22/mvvm-made-easy/
 
         [ObservableProperty]
-        private List<string> _educationLevels = new EducationLevels().AllEducationLevels;
+        private Student _selectedStudent;
 
         [ObservableProperty]
+        private ObservableCollection<string> _educationLevels = new ObservableCollection<string>(new EducationLevels().AllEducationLevels);
+
+    
         private string _selectedEducationLevel = string.Empty;
 
         [ObservableProperty]
-        private List<Student> _students = new List<Student>();        
+        private ObservableCollection<Student> _students = new ObservableCollection<Student>();
 
         public StudentViewModel()
         {
-            student = new Student();
-            _students.Add(new Student("Elek", "Teszt", System.DateTime.Now, 9, SchoolClassType.ClassA, ""));
+            SelectedStudent = new Student();
+            //Students.Add(new Student("Elek", "Teszt", System.DateTime.Now, 9, SchoolClassType.ClassA, ""));
+        }
+
+        [RelayCommand]
+        public void DoSave(Student newStudent)
+        {
+            Students.Add(newStudent);
+            OnPropertyChanged(nameof(Students));
+        }
+
+        [RelayCommand]
+        public void DoRemove(Student studentToDelete)
+        {
+            Students.Remove(studentToDelete);
+            OnPropertyChanged(nameof(Students));            
+        }
+
+        [RelayCommand] void DoNewStudent()
+        {
+            SelectedStudent = new Student();
         }
     }
 }
